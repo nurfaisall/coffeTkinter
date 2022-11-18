3d6df1cc6474fb07d7a401295df3f3bd9a456542
nurfaisall <116983574+nurfaisall@users.noreply.github.com>
2022-11-18 23:16:32 +0700

    add coffeeTkinter

---
+++ coffeeTkinter.py
@@ -0,0 +1,9 @@
+# !/usr/bin/python3  
+from tkinter import *
+#creating the application main window.   
+top = Tk()
+
+redButton = Button(top,text="lorem ipsum",fg="red")
+redButton.pack(side=RIGHT)
+
+top.mainloop()
